efb2f3b5c2e5a134467adbbb766b38554c2f4b07
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-10-10 18:54:12 -0300

    feat: add logs in NewAuthorController

--- src/main/kotlin/br/com/lukinhasssss/casa_do_codigo/controllers/NewAuthorController.kt
+++ src/main/kotlin/br/com/lukinhasssss/casa_do_codigo/controllers/NewAuthorController.kt
@@ -2,6 +2,7 @@ package br.com.lukinhasssss.casa_do_codigo.controllers
 
 import br.com.lukinhasssss.casa_do_codigo.dto.request.NewAuthorRequest
 import br.com.lukinhasssss.casa_do_codigo.repositories.AuthorRepository
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,12 +17,17 @@ class NewAuthorController(
     private val authorRepository: AuthorRepository
 ) {
 
+    private val logger = LoggerFactory.getLogger(NewAuthorController::class.java)
+
     @PostMapping
     fun registerAuthor(@RequestBody @Valid request: NewAuthorRequest): ResponseEntity<Unit> {
+        logger.info("Iniciando POST na rota: /authors - Body: $request")
+
         val author = request.toModel().let { authorRepository.save(it) }
 
         val uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(author.id).toUri()
 
+        logger.info("Finalizando POST na rota: /authors")
         return ResponseEntity.created(uri).build()
     }
 
