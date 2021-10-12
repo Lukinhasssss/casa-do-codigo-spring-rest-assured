10802e6a66b74d30ba6535c44e06b44332a93902
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-10-12 20:52:52 -0300

    style: remove unused imports

--- src/test/kotlin/br/com/lukinhasssss/casa_do_codigo/controllers/NewAuthorControllerTest.kt
+++ src/test/kotlin/br/com/lukinhasssss/casa_do_codigo/controllers/NewAuthorControllerTest.kt
@@ -10,11 +10,9 @@ import org.hamcrest.core.IsEqual
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
