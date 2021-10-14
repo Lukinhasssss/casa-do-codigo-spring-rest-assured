45dfa377aea7288dd437babca1355fcc4523e3fa
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-10-14 20:22:24 -0300

    test: add test for ListAllBooksController and ensure that returns all book list

---
+++ src/test/kotlin/br/com/lukinhasssss/casa_do_codigo/controllers/ListAllBooksControllerTest.kt
@@ -0,0 +1,75 @@
+package br.com.lukinhasssss.casa_do_codigo.controllers
+
+import br.com.lukinhasssss.casa_do_codigo.dto.request.NewAuthorRequest
+import br.com.lukinhasssss.casa_do_codigo.dto.request.NewBookRequest
+import br.com.lukinhasssss.casa_do_codigo.dto.request.NewCategoryRequest
+import br.com.lukinhasssss.casa_do_codigo.repositories.AuthorRepository
+import br.com.lukinhasssss.casa_do_codigo.repositories.BookRepository
+import br.com.lukinhasssss.casa_do_codigo.repositories.CategoryRepository
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.hamcrest.Matchers.notNullValue
+import org.hamcrest.core.IsEqual
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class ListAllBooksControllerTest {
+
+    @LocalServerPort
+    private var port = 0
+
+    @Autowired
+    lateinit var bookRepository: BookRepository
+
+    @Autowired
+    lateinit var authorRepository: AuthorRepository
+
+    @Autowired
+    lateinit var categoryRepository: CategoryRepository
+
+    @Test
+    internal fun `should return 200 with a list of all books`() {
+        // Arrange
+        authorRepository.save(authorRequest.toModel(authorRepository))
+        categoryRepository.save(categoryRequest.toModel(categoryRepository))
+        bookRepository.save(bookRequest.toModel(authorRepository, categoryRepository))
+        bookRepository.save(bookRequest.toModel(authorRepository, categoryRepository).copy(title = "Book 2", isbn = "987-654-321"))
+
+        // Act - Assert
+        Given {
+            port(port)
+        } When {
+            get("/api/v1/books")
+        } Then {
+            statusCode(200)
+            body("size()", IsEqual(2))
+            body("get(0).id", notNullValue())
+            body("get(0).name", notNullValue())
+            body("get(1).id", notNullValue())
+            body("get(1).name", notNullValue())
+        }
+    }
+
+    private val authorRequest = NewAuthorRequest(name = "Monkey D. Luffy", email = "luffy@gmail.com", description = "Mugiwara")
+    private val categoryRequest = NewCategoryRequest(name = "Programming")
+    private val bookRequest = NewBookRequest(
+        title = "Book 1",
+        resume = "Resume of Book 1",
+        summary = "Summary of Book 1",
+        price = BigDecimal("279.99"),
+        pageQuantity = 8001,
+        isbn = "123-456-789",
+        publicationDate = LocalDate.MAX,
+        authorName = authorRequest.name,
+        category = categoryRequest.name
+    )
+}
